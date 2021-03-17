41346ce787b6180d5942608162ae6ddc92c97815
crouchr <richard.crouch100@gmail.com>
2021-03-17 23:26:51 +0000

    Poll the config file to allow BTC to be added as they are purchased

--- app/bcnd.py
+++ app/bcnd.py
@@ -5,10 +5,22 @@ import time
 import requests
 import json
 from pprint import pprint
+import yaml     # install as PyYaml
 
 import get_env
 import get_env_app
 import send_metrics_to_telegraf
+
+
+def get_bcnd_config(bcnd_config_filename):
+
+    with open(bcnd_config_filename) as f:
+        bcnd_vars = yaml.load(f, Loader=yaml.FullLoader)
+    f.close()
+
+    return bcnd_vars
+
+# btc, btc_invested, min_rate, max_rate = get_bcnd_config()
 
 
 def get_bcn_price():
@@ -45,17 +57,25 @@ def get_bcn_price():
 def main():
     version = get_env.get_version()
     verbose = get_env.get_verbose()
-    btc = get_env_app.get_num_bitcoin()
-    btc_invested = get_env_app.get_gbp_invested()
+    stage = get_env.get_stage()
+
+    # btc = get_env_app.get_num_bitcoin()
+    # btc_invested = get_env_app.get_gbp_invested()
+
     telegraf_endpoint_host = get_env.get_telegraf_endpoint()    # can be read from ENV
 
     poll_secs = get_env_app.get_poll_secs()
     max_rate = -9999999
     min_rate = 999999
+    if stage == 'PRD':
+        bcnd_config_filename = '/data/btc/bcnd.yml'
+    else:
+        bcnd_config_filename = '../data/btc/bcnd.yml'
 
     print('bcnd started, version=' + version)
     print('verbose=' + verbose.__str__())
-    print('bitcoin_invested=' + btc_invested.__str__())
+    print('stage=' + stage.__str__())
+    print('bcnd_config_filename=' + bcnd_config_filename)
     print('telegraf_endpoint_host=' + telegraf_endpoint_host)
     print('poll_secs=' + poll_secs.__str__())
 
@@ -69,6 +89,12 @@ def main():
                 time.sleep(60)
                 continue            # go to start of loop
 
+            bcnd_vars = get_bcnd_config(bcnd_config_filename)
+            btc = float(bcnd_vars['num_btc'])
+            gbp_invested = float(bcnd_vars['gbp_invested'])
+            high_alarm = int(bcnd_vars['high_alarm'])
+            low_alarm = int(bcnd_vars['low_alarm'])
+
             btc_in_gbp = float(btc) * bcn_info['GBP']      # what are my BTC worth in GBP
 
             if bcn_info['GBP'] < min_rate:
@@ -78,12 +104,14 @@ def main():
                 max_rate = bcn_info['GBP']
                 max_rate_time = time.ctime()
 
-            print(time.ctime() +\
+            return_percent = round(100 * btc_in_gbp / gbp_invested, 2)
+
+            print(time.ctime() + \
                   ' (updated at ' + bcn_info['updateduk'] + ')' + \
-                  ' : BTC rate (GBP) = ' + bcn_info['GBP'].__str__() +\
+                  ' : BTC rate (GBP) = ' + bcn_info['GBP'].__str__() + \
                   ', BTC = ' + btc.__str__() + \
-                  ', BTC invested = £' + round(btc_invested,2).__str__() + \
-                  ', => GBP value = £' + round(btc_in_gbp, 2).__str__()
+                  ', BTC invested = £' + round(gbp_invested, 2).__str__() + \
+                  ' : => GBP value = £' + round(btc_in_gbp, 2).__str__()
                   )
 
             # Construct the metric bundle
@@ -94,13 +122,17 @@ def main():
                     'bitcoin_gbp': bcn_info['GBP'],
                     'bitcoin_usd': bcn_info['USD'],
                     'bitcoin_eur': bcn_info['EUR'],
-                    'btc_min_rate' : min_rate,
+                    'btc_min_rate': min_rate,
                     'btc_max_rate': max_rate,
-                    'btc_invested': btc_invested
+                    'btc_invested': gbp_invested,
+                    'return_percent': return_percent,
+                    'high_alarm': high_alarm,
+                    'low_alarm': low_alarm,
             }
+
             pprint(metrics)
 
-            send_metrics_to_telegraf.send_metrics(telegraf_endpoint_host, metrics, verbose)
+            # send_metrics_to_telegraf.send_metrics(telegraf_endpoint_host, metrics, verbose)
 
             time.sleep(poll_secs)
 
--- app/get_env_app.py
+++ app/get_env_app.py
@@ -6,31 +6,32 @@ def get_poll_secs():
         poll_secs = os.environ['POLL_SECS']
     else:
         poll_secs = 300     # same as polling OpenWeather API
+        poll_secs = 60  # same as polling OpenWeather API
 
     return poll_secs
 
 
-def get_num_bitcoin():
-    """
-    Return the number of BTC I own
-    :return:
-    """
-    if 'BITCOIN' in os.environ:
-        bitcoin_investment = os.environ['BITCOIN']
-    else:
-        bitcoin_investment = 0.00057766     # How many bitcoin do I own
+# def get_num_bitcoin():
+#     """
+#     Return the number of BTC I own
+#     :return:
+#     """
+#     if 'BITCOIN' in os.environ:
+#         bitcoin_investment = os.environ['BITCOIN']
+#     else:
+#         bitcoin_investment = 0.00057766     # How many bitcoin do I own
+#
+#     return bitcoin_investment
 
-    return bitcoin_investment
 
-
-def get_gbp_invested():
-    """
-    Return the number of GBP I have put into BTC
-    :return:
-    """
-    if 'GBP_INVESTED' in os.environ:
-        bitcoin_invested = os.environ['GBP_INVESTED']
-    else:
-        bitcoin_invested = 25.00     # How many bitcoin do I own
-
-    return bitcoin_invested
+# def get_gbp_invested():
+#     """
+#     Return the number of GBP I have put into BTC
+#     :return:
+#     """
+#     if 'GBP_INVESTED' in os.environ:
+#         bitcoin_invested = os.environ['GBP_INVESTED']
+#     else:
+#         bitcoin_invested = 48.51        # How many bitcoin do I own
+#
+#     return bitcoin_invested
